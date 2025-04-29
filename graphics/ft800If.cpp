547510ad14b12c88dc5d21bab803d75c29a9a790
Dr. Richard M. Day <richard_day@highlandbiosciences.com>
2025-04-29 00:13:01 +0000

    Refactored FT800 interface layer to use the SPI interface

--- graphics/ft800If.cpp
+++ graphics/ft800If.cpp
@@ -1,398 +1,148 @@
 /*!
- * \file ft800_if.h
- * \brief FT800 Interface implementation
+ * \file ft800If.cpp
+ * \brief FT800 SPI Interface Implementation
  */
 
  #include <stdint.h>
  #include <stdio.h>
- 
  #include "spiIf.h"
  #include "ft800If.h"
  
- Ft800_interface::Ft800_interface (uint8_t bus_number, uint8_t channel_number) : spi_interface (bus_number, channel_number)
+ Ft800_interface::Ft800_interface(uint8_t bus_number, uint8_t channel_number)
+     : spi_interface(bus_number, channel_number)
  {
-     spi_interface.enable ();
+     spi_interface.enable();
  }
  
- Ft800_interface::~Ft800_interface (void)
+ Ft800_interface::~Ft800_interface(void)
  {
-     spi_interface.disable ();
+     spi_interface.disable();
  }
  
- bool Ft800_interface::read_8_bit (uint32_t address, uint8_t* data)
+ int Ft800_interface::read_8_bit(uint32_t address, uint8_t* data)
  {
-     bool result = false;
+     if (!data) return -1;
  
-     if (NULL != data)
+     uint8_t command_buffer[4] = {
+         (uint8_t)((address >> 16) & 0x3F) | memory_read_transaction_bits,
+         (uint8_t)(address >> 8),
+         (uint8_t)(address),
+         dummy_byte_value
+     };
+ 
+     uint8_t response[1] = { 0 };
+     if (spi_interface.write_then_read(command_buffer, sizeof(command_buffer), response, sizeof(response)))
      {
-         uint8_t response_buffer[1];
- 
-         uint8_t command_buffer[4];
-         command_buffer[0] = (uint8_t)((address >> 16) & 0x3F) | memory_read_transaction_bits;
-         command_buffer[1] = (uint8_t)((address >>  8) & 0xFF);
-         command_buffer[2] = (uint8_t)((address >>  0) & 0xFF);
-         command_buffer[3] = dummy_byte_value;
- 
-         if (spi_interface.write_then_read (command_buffer, sizeof (command_buffer),
-                                            response_buffer, sizeof (response_buffer)))
-         {
-             *data = response_buffer[0];
-             result = true;
-         }
+         *data = response[0];
+         return 0;
      }
  
-     return result;
+     return -1;
  }
  
- bool Ft800_interface::read_16_bit (uint32_t address, uint16_t* data)
+ int Ft800_interface::read_16_bit(uint32_t address, uint16_t* data)
  {
-     bool result = false;
+     if (!data) return -1;
  
-     if (NULL != data)
+     uint8_t command_buffer[4] = {
+         (uint8_t)((address >> 16) & 0x3F) | memory_read_transaction_bits,
+         (uint8_t)(address >> 8),
+         (uint8_t)(address),
+         dummy_byte_value
+     };
+ 
+     uint8_t response[2] = { 0 };
+     if (spi_interface.write_then_read(command_buffer, sizeof(command_buffer), response, sizeof(response)))
      {
-         uint8_t response_buffer[2];
- 
-         uint8_t command_buffer[4];
-         command_buffer[0] = (uint8_t)((address >> 16) & 0x3F) | memory_read_transaction_bits;
-         command_buffer[1] = (uint8_t)((address >>  8) & 0xFF);
-         command_buffer[2] = (uint8_t)((address >>  0) & 0xFF);
-         command_buffer[3] = dummy_byte_value;
- 
-         if (spi_interface.write_then_read (command_buffer, sizeof (command_buffer),
-                                            response_buffer, sizeof (response_buffer)))
-         {
-             *data = (uint16_t)response_buffer[0];
-             *data |= (((uint16_t)response_buffer[1]) << 8);
-             result = true;
-         }
-     }
-     
-     return result;
- }
- 
- bool Ft800_interface::read_32_bit (uint32_t address, uint32_t* data)
- {
-     bool result = false;
- 
-     if (NULL != data)
-     {
-         uint8_t response_buffer[4];
- 
-         uint8_t command_buffer[4];
-         command_buffer[0] = (uint8_t)((address >> 16) & 0x3F) | memory_read_transaction_bits;
-         command_buffer[1] = (uint8_t)((address >>  8) & 0xFF);
-         command_buffer[2] = (uint8_t)((address >>  0) & 0xFF);
-         command_buffer[3] = dummy_byte_value;
- 
-         if (spi_interface.write_then_read (command_buffer, sizeof (command_buffer),
-                                            response_buffer, sizeof (response_buffer)))
-         {
-             *data  = response_buffer[0];
-             *data |= (((uint32_t)response_buffer[1]) <<  8);
-             *data |= (((uint32_t)response_buffer[2]) << 16);
-             *data |= (((uint32_t)response_buffer[3]) << 24);
-             result = true;
-         }
-     }
-     
-     return result;
- }
- 
- bool Ft800_interface::write_8_bit (uint32_t address, uint8_t data)
- {
-     bool result = false;
- 
-     uint8_t command_buffer[4];
-     command_buffer[0] = (uint8_t)((address >> 16) & 0x3F) | memory_write_transaction_bits;
-     command_buffer[1] = (uint8_t)((address >>  8) & 0xFF);
-     command_buffer[2] = (uint8_t)((address >>  0) & 0xFF);
-     command_buffer[3] = data;
- 
-     if (spi_interface.write (command_buffer, sizeof (command_buffer)))
-     {
-         result = true;
+         *data = (response[1] << 8) | response[0];
+         return 0;
      }
  
-     return result;
+     return -1;
  }
  
- bool Ft800_interface::write_16_bit (uint32_t address, uint16_t data)
+ int Ft800_interface::read_32_bit(uint32_t address, uint32_t* data)
  {
-     bool result = false;
+     if (!data) return -1;
  
-     uint8_t command_buffer[5];
-     command_buffer[0] = (uint8_t)((address >> 16) & 0x3F) | memory_write_transaction_bits;
-     command_buffer[1] = (uint8_t)((address >>  8) & 0xFF);
-     command_buffer[2] = (uint8_t)((address >>  0) & 0xFF);
-     command_buffer[3] = (uint8_t)((data >> 0) & 0xFF);
-     command_buffer[4] = (uint8_t)((data >> 8) & 0xFF);
+     uint8_t command_buffer[4] = {
+         (uint8_t)((address >> 16) & 0x3F) | memory_read_transaction_bits,
+         (uint8_t)(address >> 8),
+         (uint8_t)(address),
+         dummy_byte_value
+     };
  
-     if (spi_interface.write (command_buffer, sizeof (command_buffer)))
+     uint8_t response[4] = { 0 };
+     if (spi_interface.write_then_read(command_buffer, sizeof(command_buffer), response, sizeof(response)))
      {
-         result = true;
+         *data = (response[3] << 24) | (response[2] << 16) | (response[1] << 8) | response[0];
+         return 0;
      }
  
-     return result;
+     return -1;
  }
  
- bool Ft800_interface::write_32_bit (uint32_t address, uint32_t data)
+ int Ft800_interface::write_8_bit(uint32_t address, uint8_t data)
  {
-     bool result = false;
+     uint8_t command_buffer[4] = {
+         (uint8_t)((address >> 16) & 0x3F) | memory_write_transaction_bits,
+         (uint8_t)(address >> 8),
+         (uint8_t)(address),
+         data
+     };
  
-     uint8_t command_buffer[7];
-     command_buffer[0] = (uint8_t)((address >> 16) & 0x3F) | memory_write_transaction_bits;
-     command_buffer[1] = (uint8_t)((address >>  8) & 0xFF);
-     command_buffer[2] = (uint8_t)((address >>  0) & 0xFF);
-     command_buffer[3] = (uint8_t)((data >>  0) & 0xFF);
-     command_buffer[4] = (uint8_t)((data >>  8) & 0xFF);
-     command_buffer[5] = (uint8_t)((data >> 16) & 0xFF);
-     command_buffer[6] = (uint8_t)((data >> 24) & 0xFF);
+     return spi_interface.write(command_buffer, sizeof(command_buffer)) ? 0 : -1;
+ }
  
-     if (spi_interface.write (command_buffer, sizeof (command_buffer)))
+ int Ft800_interface::write_16_bit(uint32_t address, uint16_t data)
+ {
+     uint8_t command_buffer[5] = {
+         (uint8_t)((address >> 16) & 0x3F) | memory_write_transaction_bits,
+         (uint8_t)(address >> 8),
+         (uint8_t)(address),
+         (uint8_t)(data),
+         (uint8_t)(data >> 8)
+     };
+ 
+     return spi_interface.write(command_buffer, sizeof(command_buffer)) ? 0 : -1;
+ }
+ 
+ int Ft800_interface::write_32_bit(uint32_t address, uint32_t data)
+ {
+     uint8_t command_buffer[7] = {
+         (uint8_t)((address >> 16) & 0x3F) | memory_write_transaction_bits,
+         (uint8_t)(address >> 8),
+         (uint8_t)(address),
+         (uint8_t)(data),
+         (uint8_t)(data >> 8),
+         (uint8_t)(data >> 16),
+         (uint8_t)(data >> 24)
+     };
+ 
+     return spi_interface.write(command_buffer, sizeof(command_buffer)) ? 0 : -1;
+ }
+ 
+ int Ft800_interface::write(uint32_t address, const uint8_t* data, size_t length)
+ {
+     if (!data || length == 0) return -1;
+ 
+     uint8_t command[3] = {
+         (uint8_t)((address >> 16) & 0x3F) | memory_write_transaction_bits,
+         (uint8_t)(address >> 8),
+         (uint8_t)(address)
+     };
+ 
+     if (spi_interface.write(command, sizeof(command)))
      {
-         result = true;
+         return spi_interface.write(data, length) ? 0 : -1;
      }
  
-     return result;
+     return -1;
  }
  
- bool Ft800_interface::host_command (command_t command)
+ int Ft800_interface::host_command(command_t command)
  {
-     bool result = false;
- 
-     uint8_t command_buffer[3];
-     command_buffer[0] = (uint8_t)command;
-     command_buffer[1] = dummy_byte_value;
-     command_buffer[2] = dummy_byte_value;
- 
-     if (spi_interface.write (command_buffer, sizeof (command_buffer)))
-     {
-         result = true;
-     }
- 
-     return result;
+     uint8_t command_buffer[3] = { (uint8_t)command, dummy_byte_value, dummy_byte_value };
+     return spi_interface.write(command_buffer, sizeof(command_buffer)) ? 0 : -1;
  }
- 
- uint32_t Ft800_interface::clear_color_rgb (uint8_t red, uint8_t green, uint8_t blue)
- {
-     uint32_t result = clear_color_rgb_bits;
- 
-     result |= ((uint32_t)red << 16);
-     result |= ((uint32_t)green << 8);
-     result |= ((uint32_t)blue << 0);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::macro (uint8_t macro_id)
- {
-     uint32_t result = macro_bits;
- 
-     result |= ((uint32_t)macro_id & 0x01);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::clear (uint8_t color, uint8_t stencil, uint8_t tag)
- {
-     uint32_t result = clear_bits;
- 
-     if (0 != color)
-     {
-         result |= clear_color_buffer_bit;
-     }
-     if (0 != stencil)
-     {
-         result |= clear_stencil_bit;
-     }
-     if (0 != tag)
-     {
-         result |= clear_tag_bit;
-     }
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::color_rgb (uint8_t red, uint8_t green, uint8_t blue)
- {
-     uint32_t result = color_rgb_bits;
- 
-     result |= ((uint32_t)red << 16);
-     result |= ((uint32_t)green << 8);
-     result |= ((uint32_t)blue << 0);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::color_alpha (uint8_t alpha)
- {
-     uint32_t result = color_alpha_bits;
- 
-     result |= ((uint32_t)alpha);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::begin (Begin_primative_t primative)
- {
-     uint32_t result = begin_bits;
- 
-     result |= (uint32_t)primative;
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::vertex2f (int32_t x, int32_t y)
- {
-     uint32_t result = vertex2f_bits;
- 
-     result |= ((uint32_t)(x & 0x7FFF)) << 15;
-     result |= ((uint32_t)(y & 0x7FFF));
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::vertex2ii (uint16_t x, uint16_t y, uint8_t handle, uint8_t cell)
- {
-     uint32_t result = vertex2ii_bits;
- 
-     result |= ((uint32_t)(x & 0x1FF)) << 21;
-     result |= ((uint32_t)(y & 0x1FF)) << 12;
-     result |= ((uint32_t)(handle & 0x1F)) << 7;
-     result |= (uint32_t)(cell & 0x7F);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::line_width (uint16_t width)
- {
-     uint32_t result = line_width_bits;
- 
-     result |= (uint32_t)(width & 0xFFF);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::point_size (uint16_t size)
- {
-     uint32_t result = point_size_bits;
- 
-     result |= (uint32_t)(size & 0xFFF);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::bitmap_source (uint32_t address)
- {
-     uint32_t result = bitmap_source_bits;
- 
-     result |= (address & 0xFFFFF);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::bitmap_layout (uint16_t format, uint16_t stride, uint16_t height)
- {
-     uint32_t result = bitmap_layout_bits;
- 
-     result |= (format & 0x1F) << 19;
-     result |= (stride & 0x3FF) << 9;
-     result |= (height & 0x1FF);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::bitmap_size (uint8_t filter, uint8_t wrap_x, uint8_t wrap_y, uint16_t width, uint16_t height)
- {
-     uint32_t result = bitmap_size_bits;
- 
-     result |= (filter & 1) << 20;
-     result |= (wrap_x & 1) << 19;
-     result |= (wrap_y & 1) << 18;
-     result |= (width & 0x1FF) << 9;
-     result |= (height & 0x1FF);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::bitmap_handle (uint8_t handle)
- {
-     uint32_t result = bitmap_handle_bits;
- 
-     result |= (handle & 0x1F);
- 
-     return result;
- }
- 
- uint32_t Ft800_interface::tag (uint8_t value)
- {
-     uint32_t result = tag_bits;
- 
-     result |= value;
- 
-     return result;
- }
- 
- #ifdef PERFORM_FT800_INTERFACE_TESTS
- 
- int main (void)
- {
-     Ft800_interface ft800_interface (1, 0);
-     uint8_t result_8;
-     uint16_t result_16;
-     uint32_t result_32;
- 
-     if (ft800_interface.read_8_bit (0, &result_8))
-     {
-         printf ("Read  8: %02X\n", result_8);
-     }
-     else
-     {
-         printf ("read_8_bit failed\n");
-     }
-     if (ft800_interface.read_16_bit (0x100, &result_16))
-     {
-         printf ("Read 16: %04X\n", result_16);
-     }
-     else
-     {
-         printf ("read_16_bit failed\n");
-     }
-     if (ft800_interface.read_32_bit (0x20000, &result_32))
-     {
-         printf ("Read 32: %08X\n", result_32);
-     }
-     else
-     {
-         printf ("read_32_bit failed\n");
-     }
- 
-     if (ft800_interface.write_8_bit (0x0, 0xAB))
-     {
-         printf ("write_8_bit OK\n");
-     }
-     else
-     {
-         printf ("write_8_bit failed\n");
-     }
-     if (ft800_interface.write_16_bit (0x800, 0xABCD))
-     {
-         printf ("write_16_bit OK\n");
-     }
-     else
-     {
-         printf ("write_16_bit failed\n");
-     }
-     if (ft800_interface.write_32_bit (0x200000, 0xABCDEF55))
-     {
-         printf ("write_32_bit OK\n");
-     }
-     else
-     {
-         printf ("write_32_bit failed\n");
-     }
- 
-     return 0;
- }    
- #endif
  
--- graphics/ft800If.h
+++ graphics/ft800If.h
@@ -1,6 +1,6 @@
 /*!
- * \file ft800_if.h
- * \brief FT800 Interface header file
+ * \file ft800If.h
+ * \brief FT800 SPI Interface Header
  */
 
  #ifndef FT800_IF_H
@@ -8,9 +8,14 @@
  
  #include "spiIf.h"
  
+ /*!
+  * \class Ft800_interface
+  * \brief Provides FT800-specific SPI memory and command access.
+  */
  class Ft800_interface
  {
  public:
+     //! Host commands for FT800 device
      typedef enum
      {
          ACTIVE  = 0x00,
@@ -23,59 +28,63 @@
          CORERST = 0x68
      } command_t;
  
+     //! Primitive types for graphics engine
      typedef enum 
      {
          BITMAPS = 1,
-         POINTS = 2,
-         LINES = 3,
-         LINE_STRIP = 4,
-         EDGE_STRIP_R = 5,
-         EDGE_STRIP_L = 6,
-         EDGE_STRIP_A = 7,
-         EDGE_STRIP_B = 8,
-         RECTS = 9
-     } Begin_primative_t;
+         POINTS,
+         LINES,
+         LINE_STRIP,
+         EDGE_STRIP_R,
+         EDGE_STRIP_L,
+         EDGE_STRIP_A,
+         EDGE_STRIP_B,
+         RECTS
+     } Begin_primitive_t;
  
-     Ft800_interface (uint8_t bus_number, uint8_t channel_number);
-     ~Ft800_interface (void);
+     Ft800_interface(uint8_t bus_number, uint8_t channel_number);
+     ~Ft800_interface(void);
  
-     bool read_8_bit (uint32_t address, uint8_t* data);
-     bool read_16_bit (uint32_t address, uint16_t* data);
-     bool read_32_bit (uint32_t address, uint32_t* data);
+     int read_8_bit(uint32_t address, uint8_t* data);
+     int read_16_bit(uint32_t address, uint16_t* data);
+     int read_32_bit(uint32_t address, uint32_t* data);
  
-     bool write_8_bit (uint32_t address, uint8_t data);
-     bool write_16_bit (uint32_t address, uint16_t data);
-     bool write_32_bit (uint32_t address, uint32_t data);
+     int write_8_bit(uint32_t address, uint8_t data);
+     int write_16_bit(uint32_t address, uint16_t data);
+     int write_32_bit(uint32_t address, uint32_t data);
+     int write(uint32_t address, const uint8_t* data, size_t length); //!< Write arbitrary block of data to FT800 memory
  
-     bool host_command (command_t command);
+     int host_command(command_t command);
  
-     uint32_t clear_color_rgb (uint8_t red, uint8_t green, uint8_t blue);
-     uint32_t clear (uint8_t color, uint8_t stencil, uint8_t tag);
-     uint32_t display (void) { return display_bits; };
-     uint32_t color_rgb (uint8_t red, uint8_t green, uint8_t blue);
-     uint32_t color_alpha (uint8_t alpha);
+     uint32_t clear_color_rgb(uint8_t red, uint8_t green, uint8_t blue);
+     uint32_t clear(uint8_t color, uint8_t stencil, uint8_t tag);
+     uint32_t display(void) { return display_bits; }
+     uint32_t color_rgb(uint8_t red, uint8_t green, uint8_t blue);
+     uint32_t color_alpha(uint8_t alpha);
  
-     uint32_t begin (Begin_primative_t primative);
-     uint32_t end (void) { return end_bits; };
-     uint32_t vertex2f (int32_t x, int32_t y);
-     uint32_t vertex2ii (uint16_t x, uint16_t y, uint8_t handle, uint8_t cell);
+     uint32_t begin(Begin_primitive_t primitive);
+     uint32_t end(void) { return end_bits; }
+     uint32_t vertex2f(int32_t x, int32_t y);
+     uint32_t vertex2ii(uint16_t x, uint16_t y, uint8_t handle, uint8_t cell);
  
-     uint32_t point_size (uint16_t size);
-     uint32_t line_width (uint16_t width);
+     uint32_t point_size(uint16_t size);
+     uint32_t line_width(uint16_t width);
  
-     uint32_t macro (uint8_t macro_id);
+     uint32_t macro(uint8_t macro_id);
  
-     uint32_t bitmap_source (uint32_t address);
-     uint32_t bitmap_layout (uint16_t format, uint16_t stride, uint16_t height);
-     uint32_t bitmap_size (uint8_t filter, uint8_t wrap_x, uint8_t wrap_y, uint16_t width, uint16_t height);
-     uint32_t bitmap_handle (uint8_t handle);
+     uint32_t bitmap_source(uint32_t address);
+     uint32_t bitmap_layout(uint16_t format, uint16_t stride, uint16_t height);
+     uint32_t bitmap_size(uint8_t filter, uint8_t wrap_x, uint8_t wrap_y, uint16_t width, uint16_t height);
+     uint32_t bitmap_handle(uint8_t handle);
  
-     uint32_t tag (uint8_t value);
+     uint32_t tag(uint8_t value);
  
-     uint32_t save_context (void) { return save_context_bits; };
-     uint32_t restore_context (void) { return restore_context_bits; };
+     uint32_t save_context(void) { return save_context_bits; }
+     uint32_t restore_context(void) { return restore_context_bits; }
  
  private:
+     Spi_interface spi_interface; //!< Low-level SPI interface instance
+ 
      static const uint8_t memory_read_transaction_bits  = 0x00;
      static const uint8_t memory_write_transaction_bits = 0x80;
      static const uint8_t dummy_byte_value = 0x00;
@@ -100,18 +109,9 @@
      static const uint32_t end_bits = 0x21000000;
      static const uint32_t vertex2f_bits = 0x40000000;
      static const uint32_t vertex2ii_bits = 0x80000000;
- 
-     static const uint32_t point_size_bits = 0x0D000000;
-     static const uint32_t line_width_bits = 0x0E000000;
- 
-     static const uint32_t macro_bits = 0x25000000;
- 
-     static const uint32_t save_context_bits = 0x22000000;
-     static const uint32_t restore_context_bits = 0x23000000;
- 
-     Spi_interface spi_interface;
- 
+     static const uint32_t save_context_bits = 0x0D000000;
+     static const uint32_t restore_context_bits = 0x0E000000;
  };
  
- #endif
+ #endif // FT800_IF_H
  
